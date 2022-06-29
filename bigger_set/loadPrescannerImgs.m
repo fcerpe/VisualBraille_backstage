6bef95158a1092cf33869c3da2733a49efd27f41
Filippo Cerpelloni <filippo.cerpelloni@gmail.com>
2022-06-29 15:41:44 +0200

    final changes to prescan imgs

--- bigger_set/loadPrescannerImgs.m
+++ bigger_set/loadPrescannerImgs.m
@@ -4,3 +4,8 @@ prescanner_french = imread('../../../../Downloads/prescanner_french.png');
 
 save('prescan_imgs.mat');
 save('../../VisualBraille/inputs/prescan_imgs.mat');
+
+%% Not chronologically oredered: 
+% randomize stimuli:
+frOrder = repmat(["frw", "fpw", "fnw"], 1, 12);
+frShuffle = shuffle(frOrder);
